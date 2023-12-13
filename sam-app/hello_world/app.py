7564cd9e808748d0e887484c6bee6b865ffbf171
ryellreddy <ryell@my.bridgeport.edu>
2023-12-13 01:41:37 -0500

    text update

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "i am glad to have complete this tutorial",
             # "location": ip.text.replace("\n", "")
         }),
     }
